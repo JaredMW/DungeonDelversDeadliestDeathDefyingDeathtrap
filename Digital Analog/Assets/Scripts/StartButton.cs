5cd00b77b105c280d0536b865ba0a6fa04eb6aba
jcb5944 <jcb5944@IGM2000-11>
2017-11-10 13:44:05 -0500

    Shit got done

---
+++ Digital Analog/Assets/Scripts/StartButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+
+	 public void StartGame()
+    {
+        Debug.Log("Started");
+        SceneManager.LoadScene("TestScene");
+    }
+    public void StartGames()
+    {
+        Debug.Log("Started");
+        SceneManager.LoadScene("Game");
+    }
+}
---
+++ Digital Analog/Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour {
+private static int count;
+public Text texting;
+	// Use this for initialization
+	void Start () {
+        //count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        count++;
+        texting.text = count.ToString();
+	}
+
+    void onGUI()
+    {
+
+    }
+}
