b61b7c43210f685166eed724c07d72a250277672
ilw7159 <ilw7159@IGM2570-16>
2017-11-15 09:25:41 -0500

    Made minigame 1 link back to the game selection screen (need to change slightly if we implement QR code stuff)

--- Digital Analog/Assets/Scripts/MiniGame1manager.cs
+++ Digital Analog/Assets/Scripts/MiniGame1manager.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MiniGame1Manager : Gamemanager {
     float radius = 7.15f;
+    bool play = true;
+    float endtimer = 5;
     //public Button button;            // Push to start
     //public Text text1, text2, text3; // Menu text
 
@@ -50,7 +53,7 @@ public class MiniGame1Manager : Gamemanager {
 
             }
         }
-        if (Timer.count > 10)
+        if (Timer.count > 10 && play == true)
         {
             arena.gameObject.transform.localScale *= .999f;
             radius *= .999f;
@@ -60,13 +63,13 @@ public class MiniGame1Manager : Gamemanager {
             float distance = 0;
 
             distance = Mathf.Sqrt((Mathf.Pow(players[i].transform.position.x- arena.transform.position.x, 2) + Mathf.Pow(players[i].transform.position.y - arena.transform.position.y, 2)));
-            if (distance >radius)
+            if (distance > radius)
             {
                 players[i].SetActive(false);
                 players[i].GetComponent<Movement>().enabled = false;
                 players[i].GetComponentInChildren<SpriteRenderer>().enabled = false;
                 players.Remove(players[i]);
-                Debug.Log("player removed");
+                //Debug.Log("player removed");
                 
             }
             //Debug.Log(distance);
@@ -76,14 +79,26 @@ public class MiniGame1Manager : Gamemanager {
          // }
         }
 
-        if (players.Count == 1)
+        if (players.Count <= 1)
         {
-            GetComponent<Timer>().iscounting = false ;
-            arena.gameObject.transform.localScale /=.999f;
-            radius /= .999f;
 
+            play = false;
+            GetComponent<Timer>().iscounting = false ;
+            
+            /*arena.gameObject.transform.localScale /=.999f;
+            radius /= .999f;*/
+
+            text1.gameObject.SetActive(true);
+
+            text1.text = players[0].gameObject.tag.ToString() + " won!";
+            endtimer = endtimer - .01f;
+            if (endtimer <= 0)
+            {
+                Timer.count = 0;
+                SceneManager.LoadScene("Scanning");
+            }
         }
-        }
+    }
 
   //  public void StartMinigame()
    // {
